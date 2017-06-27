59adb68ea586008c65b42d71ea46c480860c1429
John Stewart <stewart.ja@gmail.com>
2017-06-27 09:35:12 -0500

    Create groupMeWidget.php

---
+++ groupMeWidget.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Group Me Widget
+Description: Adds a widget to the site that displays the latest posts from a GroupMe account
+*/
+/* Start Adding Functions Below this Line */
+
+
+/* Stop Adding Functions Below this Line */
+?>
